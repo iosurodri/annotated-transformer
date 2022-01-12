f886a02fed5c739cd17a46a43f26106fc4702a18
iosurodri <iosu.rodriguez@unavarra.es>
2022-01-12 16:58:23 +0100

    DEBUG: Refactoring Annotated transformer into project

---
+++ src/layers/aux_functions.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import copy
+
+def clones(module, N):
+    "Produce N identical layers."
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+def subsequent_mask(size):
+    "Mask out subsequent positions"
+    # Useful only for sentence completion tasks (?)
+    attn_shape = (1, size, size)
+    # The predictions for position i can depend only on the known outputs at positions less than i:
+    subsequent_matrix = torch.logical_not(torch.triu(torch.ones(attn_shape, dtype=torch.bool), diagonal=1))
+    return subsequent_matrix
---
+++ src/layers/layers.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn as nn
+from torch.nn.modules.normalization import LayerNorm
+from aux_functions import clones
+
+### Auxiliar layers:
+
+class LayerNorm(nn.Module):
+    "Construct a layernorm module (See https://arxiv.org/abs/1607.06450 for details)"
+    def __init__(self, features, eps=1e-6):
+        super().__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2  # self.eps avoids instabilities both in forward and backpropagation passes
+
+
+class SublayerConnection(nn.Module):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity the norm is first as opposed to last.
+    """
+    def __init__(self, size, dropout):
+        super().__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        "Apply a residual connection to any sublayer with the same size"
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+### Encoder and Decoder layers:
+
+class EncodeLayer(nn.Module):
+    "Encoder is made up of self-attn and feed forward"
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super().__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)  # Provides residual connection and normalization for each layer
+        self.size = size
+
+    def forward(self, x, mask):
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+
+class Encoder(nn.Module):
+    "Core encoder is a stack of N layers"
+    def __init__(self, layer, N):
+        super().__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        "Pass the input (and mask) through each layer in turn."
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
+class DecoderLayer(nn.Module):
+    "Decoder is made of self-attn, src-attn, and feed forward"
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        super().__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))  # The second attention mechanism takes into account the encoded output of an EncoderLayer
+        return self.sublayer[2](x, self.feed_forward)
+
+
+class Decoder(nn.Module):
+    "Generic N layer decoder with masking"
+    def __init__(self, layer, N):
+        super().__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)  # The decoder takes into account both the encoded output and the target
+        return self.norm(x)
