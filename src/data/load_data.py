efb82749d6d5a97ab5194e1b7cbe5094f5df0595
iosurodri <iosu.rodriguez@unavarra.es>
2022-01-13 17:41:31 +0100

    REFACT: NoamOpt has been moved to src.optim

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,3 +1,4 @@
+# TODO: Remove global values (?)
 global max_src_in_batch, max_tgt_in_batch
 
 def batch_size_fn(new, count, sofar):
@@ -12,34 +13,3 @@ def batch_size_fn(new, count, sofar):
     tgt_elements = count * max_tgt_in_batch
     return max(src_elements, tgt_elements)
 
-
-class NoamOpt:
-    "Optim wrapper that implements rate."
-    def __init__(self, model_size, factor, warmup, optimizer):
-        self.optimizer = optimizer
-        self._step = 0
-        self.warmup = warmup
-        self.factor = factor
-        self.model_size = model_size
-        self._rate = 0
-
-    def step(self):
-        "Update parameters and rate"
-        self._step += 1
-        rate = self.rate()
-        for p in self.optimizer.param_groups:
-            p['lr'] = rate
-        self._rate = rate
-        self.optimizer.step()
-
-    def rate(self, step=None):
-        """Implement lrate:
-        lrate = d_model^-0.5 * min(step_num^(-0.5), step_num * warmup_steps^(-1.5))
-        """
-        if step is None:
-            step = self._step
-        return self.factor * (self.model_size ** (-0.5) * min(step ** (-0.5), step * self.warmup ** (-1.5)))
-
-
-def get_std_opt(model):
-    return NoamOpt(model.src_embed[0].d_model, 2, 4000, torch.optim.Adam(model.parameters(), lr=0., betas=(0.9, 0.98), eps=1e-9))
